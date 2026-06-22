e11479c24d97fe2bae55c0b3ec55411247b9773f
Lee Jong Min <intern-lee.jeong.min@codecomplete.jp>
2026-06-23 00:13:20 +0900

    0622복습_FetchPage부터하셈

--- src/pages/LifeCyclePage.jsx
+++ src/pages/LifeCyclePage.jsx
@@ -4,20 +4,22 @@ import { useEffect, useState } from 'react'
 export default function LifeCyclePage() {
     
     const [text, setText] = useState('A'); // text 변수를 선언하고 A라는 값을 할당
-    
-    useEffect(() => {
-        alert('text가 변경됨 : ');
-    }, [text]);
-
-    // 컴포넌트가 마운트될 때 실행되는 함수
-    useEffect(() => {
-        console.log('LifeCyclePage 준비됨');
-    }, []);
 
     const handleClick = () => {
         setText('B');         // 버튼을 클릭하면 text 변수를 B로 변경
     }
     
+    // LifeCyclePage이라는 컴포넌트가 마운트될 때 실행되는 함수
+    useEffect(() => {
+        console.log('LifeCyclePage 준비됨');
+    }, []);
+    
+    // text 변수의 값이 바뀔 때 실행되는 함수
+    useEffect(() => {
+        alert('text가 변경됨 : ');
+    }, [text]);
+
+    // 화면 그리기
     return (
         <div>
             <h1>LifeCyclePage : {text}</h1>
